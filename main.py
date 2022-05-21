5df0ba1d59d7fa107d267cd762eea1e85bc15e96
haoching <89097803+haoching@users.noreply.github.com>
2022-05-21 21:58:13 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import pygame
+import ctypes
+FPS = 60
+
+pygame.init()
+
+ctypes.windll.user32.SetProcessDPIAware()
+pygame.display.set_mode((1920,1080))
+
+clock = pygame.time.Clock()
+
+
+running =True
+
+while running:
+    clock.tick(FPS)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+pygame.quit()

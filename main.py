24555fe1c7b6315f92f0fe9d84473db5a92aa775
haoching <haoching929@gmail.com>
2022-05-22 10:22:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,24 @@ import pygame
 import ctypes
 FPS = 60
 
+#初始化
 pygame.init()
-
 ctypes.windll.user32.SetProcessDPIAware()
 screen = pygame.display.set_mode((1920,1080))
+pygame.display.set_caption("電研成發第二組")
 
 clock = pygame.time.Clock()
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((100,100))
+        self.image.fill((255,255,255))
+        self.rect = self.image.get_rect()
+
+all_sprites = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
 
 running =True
 
@@ -19,6 +30,8 @@ while running:
             running = False
 
     screen.fill((135,206,235))
+    all_sprites.draw(screen)
     pygame.display.update()
+
 
 pygame.quit()

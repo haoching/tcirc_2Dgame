e80c8307b4d7d13b5a164e414f58989f43ed20bd
haoching <89097803+haoching@users.noreply.github.com>
2022-05-29 23:06:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import pygame
 import ctypes
 import random
 import os
-
+from player_1 import Player_1
+from player_2 import Player_2
 
 FPS = 60
 WHITE = (255, 255, 255)
@@ -112,85 +113,6 @@ def draw_score(surf, text, size, x, y):
     surf.blit(text_surface, text_rect)    
 
 
-#玩家1運動
-player_1_high = 200
-player_1_width = 100
-player_1_speed_y=jump_speed
-player_1_jumping=False
-class Player_1(pygame.sprite.Sprite):
-    jumping = False
-    player_1_speed_y = 100
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((player_1_width, player_1_high))
-        self.image.fill((255, 255, 255))
-        self.rect = self.image.get_rect()
-        self.rect.x = screen_width/3-player_1_width#對稱
-        self.rect.y = screen_high-player_1_high
-        self.health = 100
-
-    def update(self):
-        global player_1_speed_y
-        global player_1_jumping
-
-        key_pressed = pygame.key.get_pressed()
-        if key_pressed[pygame.K_a]:
-            self.rect.x -= player_speed
-        if key_pressed[pygame.K_d]:
-            self.rect.x += player_speed
-        if key_pressed[pygame.K_w]:
-            player_1_jumping = True
-        if player_1_jumping:
-            self.rect.y-=player_1_speed_y
-            player_1_speed_y-=gravity
-        if self.rect.y>=screen_high-player_1_high:
-            player_1_jumping = False
-            player_1_speed_y = jump_speed
-        #邊界判斷
-        if self.rect.x >= screen_width-player_1_width:
-            self.rect.x = screen_width-player_1_width
-        if self.rect.x <= 0:
-            self.rect.x = 0    
-
-
-#玩家2運動
-player_2_high = 200
-player_2_width = 100
-player_2_speed_y=jump_speed
-player_2_jumping=False
-class Player_2(pygame.sprite.Sprite):
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((player_2_width, player_2_high))
-        self.image.fill((0, 0, 0))
-        self.rect = self.image.get_rect()
-        self.rect.x=screen_width/3*2
-        self.rect.y=screen_high-player_1_high
-        self.health = 100
-
-    def update(self):
-        global player_2_speed_y
-        global player_2_jumping
-        key_pressed = pygame.key.get_pressed()
-        if key_pressed[pygame.K_j]:
-            self.rect.x -= player_speed
-        if key_pressed[pygame.K_l]:
-            self.rect.x += player_speed
-        if key_pressed[pygame.K_i]:
-            player_2_jumping=True
-        if player_2_jumping:
-            self.rect.y-=player_2_speed_y
-            player_2_speed_y-=gravity
-        if self.rect.y>=screen_high-player_2_high:
-            player_2_jumping = False
-            player_2_speed_y = jump_speed
-        #邊界判斷
-        if self.rect.x >= screen_width-player_2_width:
-            self.rect.x = screen_width-player_2_width
-        if self.rect.x <= 0:
-            self.rect.x = 0     
-                        
-
 all_sprites = pygame.sprite.Group()
 player_1 = Player_1()
 all_sprites.add(player_1)
@@ -211,7 +133,6 @@ while running:
     #
     all_sprites.update()
     
-
     #畫面顯示
     screen.fill((135, 206, 235))
     screen.blit(background_img_4 , (0,0))

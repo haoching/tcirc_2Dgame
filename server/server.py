bc6510e07d9313eafa474be1ad8a374056192480
haoching <89097803+haoching@users.noreply.github.com>
2022-05-30 23:00:09 +0800

    server

--- server/server.py
+++ server/server.py
@@ -10,8 +10,18 @@ s.bind((HOST, PORT))
 print('server start at: %s:%s' % (HOST, PORT))
 print('wait for connection...')
 
+player_1_address = 0
+player_2_address = 0
+
 while True:
     indata, address = s.recvfrom(1024)
     data = indata.decode()
-
+    if player_1_address:
+        if player_2_address:
+            print(str(player_1_address)+str(player_2_address))
+        elif player_1_address != address:
+            player_2_address = address
+    else:
+        player_1_address = address
+        
     print('recvfrom ' + str(address) + ': ' + data)

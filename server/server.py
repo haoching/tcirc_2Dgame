4fe6dc89afb498c1fe9432abf2011efd08c039c2
haoching <89097803+haoching@users.noreply.github.com>
2022-06-03 16:00:35 +0800

    Update server.py

--- server/server.py
+++ server/server.py
@@ -1,5 +1,5 @@
 import socket
-import time
+import threading
 
 #socket
 HOST = '0.0.0.0'
@@ -19,9 +19,31 @@ while True:
     if player_1_address:
         if player_2_address:
             print(str(player_1_address)+str(player_2_address))
+            break
         elif player_1_address != address:
             player_2_address = address
     else:
         player_1_address = address
-        
-    print('recvfrom ' + str(address) + ': ' + data)
+
+working = False
+
+def response(self):
+    while working == False:
+        s.sendto("connected".encode(), player_1_address)
+        s.sendto("connected".encode(), player_2_address)
+
+#多執行序
+t = threading.Thread(target=response, args=('Nash',))
+t.start() # 開始
+
+#server邏輯
+while True:
+    indata, address = s.recvfrom(1024)
+    data = indata.decode()
+    if address == player_1_address:
+        s.sendto(indata, player_2_address)
+    elif address == player_2_address:
+        s.sendto(indata, player_1_address)
+    else:
+        print("error")
+    working = True
